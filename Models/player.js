65c7d955d46922ebb4cc97911623dec66b2c52c5
Ethan Storm <stormethan10@gmail.com>
2023-03-08 17:58:01 -0800

    initial commmit

---
+++ Models/player.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const playerSchema = new mongoose.Schema({
+	email: { type: String, require: true },
+	username: { type: String, required: true },
+	stats: {
+		health: Number,
+		gold: Number,
+		AP: Number,
+	},
+	class: String,
+	map: Object,
+	position: Number,
+})
+
+module.exports = mongoose.model('Player', playerSchema)
